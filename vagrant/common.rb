593c1dc1c508e310908922959d17feed073a4965
Giles Knap <giles.knap@diamond.ac.uk>
2024-10-02 21:36:24 +0100

    support all distros simultaneously in same vagrant cache

--- vagrant/common.rb
+++ vagrant/common.rb
@@ -1,30 +1,38 @@
+# environment parameters
+@distro_name = ENV['VAGRANT_VM_BOX'] || "rocky"
+@cpus = ENV['VAGRANT_VM_CPUS'] || 4
+@ansible_args = ENV['VAGRANT_ANSIBLE_ARGS'] || ""
 
-distro_name = ENV['VAGRANT_VM_BOX'] || "debian"
-
-distros = {
-  'fedora': {
+@distros = [
+  {
     box: "fedora/40-cloud-base",
     installer: "dnf",
+    ip: "192.168.100.10",
+    name: "fedora"
   },
-  'rocky': {
+  {
     box: "rockylinux/9",
     installer: "dnf",
+    ip: "192.168.100.11",
+    name: "rocky"
   },
-  'debian': {
+  {
     box: "debian/bookworm64",
     installer: "apt",
+    ip: "192.168.100.12",
+    name: "debian"
   },
-  'ubuntu': {
+  {
     box: "ubuntu/focal64",
     installer: "apt",
+    ip: "192.168.100.13",
+    name: "ubuntu"
   },
-}
+]
 
-if not distros.key?(distro_name.to_sym)
-  print "Unknown distro: #{distro_name}, supported distros: #{distros.keys}\n"
+choices = ["fedora", "rocky", "debian", "ubuntu", "all"]
+
+if not choices.include?(@distro_name)
+  print "ERROR: allowed values for VAGRANT_VM_BOX are #{choices}"
   exit 1
 end
-
-@distro = distros[distro_name.to_sym]
-@cpus = ENV['VAGRANT_VM_CPUS'] || 4
-
