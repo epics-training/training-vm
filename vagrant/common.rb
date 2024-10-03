4d4245be9f582ca513b22fb9d3a8b128c0aa0613
Giles Knap <giles.knap@diamond.ac.uk>
2024-10-03 07:52:44 +0100

    make rocky default, support comma separated list

--- vagrant/common.rb
+++ vagrant/common.rb
@@ -1,5 +1,5 @@
 # environment parameters
-@distro_name = ENV['VAGRANT_VM_BOX'] || "rocky"
+@distro_names = (ENV['VAGRANT_VM_BOX'] || "rocky").split(',')
 @cpus = ENV['VAGRANT_VM_CPUS'] || 4
 @ansible_args = ENV['VAGRANT_ANSIBLE_ARGS'] || ""
 
@@ -7,32 +7,30 @@
   {
     box: "fedora/40-cloud-base",
     installer: "dnf",
-    ip: "192.168.100.10",
     name: "fedora"
   },
   {
     box: "rockylinux/9",
     installer: "dnf",
-    ip: "192.168.100.11",
     name: "rocky"
   },
   {
     box: "debian/bookworm64",
     installer: "apt",
-    ip: "192.168.100.12",
     name: "debian"
   },
   {
     box: "ubuntu/focal64",
     installer: "apt",
-    ip: "192.168.100.13",
     name: "ubuntu"
   },
 ]
 
 choices = ["fedora", "rocky", "debian", "ubuntu", "all"]
 
-if not choices.include?(@distro_name)
-  print "ERROR: allowed values for VAGRANT_VM_BOX are #{choices}"
+if not @distro_names.all? { |e| choices.include?(e) }
+  print "ERROR: allowed values for VAGRANT_VM_BOX are comma separate distros #{choices} or 'all'"
   exit 1
+else
+  print "INFO: VAGRANT_VM_BOX is #{@distro_names}"
 end
