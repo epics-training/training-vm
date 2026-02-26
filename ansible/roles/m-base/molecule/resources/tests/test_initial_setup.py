67cfec6dfb00c74cc9b42f2320e199c3f29319a5
Simon Rose <simon.rose@ess.eu>
2026-02-26 17:17:17 +0000

    Test that RELEASE.local is built correctly

--- ansible/roles/m-base/molecule/resources/tests/test_initial_setup.py
+++ ansible/roles/m-base/molecule/resources/tests/test_initial_setup.py
@@ -1,6 +1,15 @@
 import pytest
 
 EPICS_DEV_USER = "epics-dev"
+RELEASE_LOCAL_PATH = "/opt/epics/RELEASE.local"
+
+
+@pytest.mark.parametrize("target", ["EPICS_BASE", "PVXS"])
+def test_release_dot_local_is_populated(host, target):
+    assert any(
+        line.startswith(f"{target}=")
+        for line in host.file(RELEASE_LOCAL_PATH).content_string.splitlines()
+    )
 
 
 @pytest.mark.parametrize("executable", ["softIoc", "softIocPVA", "softIocPVX"])
