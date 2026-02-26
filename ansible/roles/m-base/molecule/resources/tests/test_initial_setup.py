7025bac67c5415abe1237ab857e603bf87ed3f24
Simon Rose <simon.rose@ess.eu>
2026-02-26 17:09:16 +0000

    Test that softIoc(s) are on path

--- ansible/roles/m-base/molecule/resources/tests/test_initial_setup.py
+++ ansible/roles/m-base/molecule/resources/tests/test_initial_setup.py
@@ -1,2 +1,10 @@
-def test_no_op(host):
-    assert True
+import pytest
+
+EPICS_DEV_USER = "epics-dev"
+
+
+@pytest.mark.parametrize("executable", ["softIoc", "softIocPVA", "softIocPVX"])
+def test_softIocXXX_is_on_path(host, executable):
+    with host.sudo(EPICS_DEV_USER):
+        cmd = host.run(f"echo exit | bash -lc '{executable}'")
+        assert cmd.succeeded
